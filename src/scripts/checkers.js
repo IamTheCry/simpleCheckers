e8ad3e1d5f3400cb977be89a0b80f90af83d56fe
Corey <cry@Coreys-MacBook-Air.local>
2017-11-02 12:39:02 -0700

    Pseudo coded the game logic and client side API calls

--- src/scripts/checkers.js
+++ src/scripts/checkers.js
@@ -1,1 +1,5 @@
 console.log('script loaded');
+
+// on click make post request to the server to check game state
+// if valid move server will alter game state and send it back to client
+// on response, will either re-render the table with the updated board or alert user of invalid move
