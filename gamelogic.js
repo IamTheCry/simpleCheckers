dce6439172569049ec5cebc679b8573c7a54c1fb
Corey <cry@Coreys-MacBook-Air.local>
2017-11-02 13:00:51 -0700

    Pseudo coded more game logic

--- gamelogic.js
+++ gamelogic.js
@@ -2,7 +2,7 @@
 // 1 = black checker
 // 2 = red checker
 
-const board = [
+const initialBoard = [
   [0,1,0,1,0,1,0,1],
   [1,0,1,0,1,0,1,0],
   [0,1,0,1,0,1,0,1],
@@ -12,4 +12,12 @@ const board = [
   [0,2,0,2,0,2,0,2],
   [2,0,2,0,2,0,2,0]
 ]
+let currentBoard;
 
+function validMoveChecker(board) {
+  // checks if the board submitted by the client is valid or not by comparing it to the current board
+  // if valid
+  return currentBoard = board
+}
+
+// send the currentBoard back to the client
